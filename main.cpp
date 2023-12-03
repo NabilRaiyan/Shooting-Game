23e5456dd3e8f591b7e8b334253e0ebf28efb3a6
Raiyan Al Sultan <rraiyan77@gmail.com>
2023-12-03 10:45:27 +0600

    Fixing bugs

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <GL/glut.h>
-#include <windows.h>
-
-
 
 // Player position
 float playerX = -18.0f;
 float playerY = -15.0f;
 
 // Bullet position
-float bulletX = 0.0f;
-float bulletY = 0.0f;
+float bulletX = 1.0f;
+float bulletY = 1.0f;
 bool isBulletActive = false;
-
+bool isPlayerMoving = true;
 
 void drawPlayer() {
     // Code to draw the player (e.g., a rectangle or sprite)
-    glColor3f(1.0f, 1.0f, 0.0f);
+    glColor3f(1.0f, 0.5f, 0.0f);
     glBegin(GL_QUADS);
     glVertex2f(playerX - 1.0f, playerY - 1.0f);
     glVertex2f(playerX + 1.0f, playerY - 1.0f);
@@ -26,20 +23,20 @@ void drawPlayer() {
 }
 
 void drawBullet() {
-     // Code to draw the bullet in red and slightly larger
+    // Code to draw the bullet in red and slightly larger
     glColor3f(1.0f, 0.0f, 0.0f);  // Red color
     glBegin(GL_QUADS);
-    glVertex2f(bulletX - 0.2f, bulletY - 0.2f);
-    glVertex2f(bulletX + 0.2f, bulletY - 0.2f);
-    glVertex2f(bulletX + 0.2f, bulletY + 0.2f);
-    glVertex2f(bulletX - 0.2f, bulletY + 0.2f);
+    glVertex2f(bulletX - 0.1f, bulletY - 0.1f);
+    glVertex2f(bulletX + 0.1f, bulletY - 0.1f);
+    glVertex2f(bulletX + 0.1f, bulletY + 0.1f);
+    glVertex2f(bulletX - 0.1f, bulletY + 0.1f);
     glEnd();
 }
 
 void update(int value) {
     // Update the bullet position if it is active
     if (isBulletActive) {
-        bulletX += 0.5f;  // Adjust the speed of the bullet
+        bulletX += 0.6f;  // Adjust the speed of the bullet
         // Check if the bullet is out of bounds
         if (bulletX > 20.0f) {
             isBulletActive = false;
@@ -47,7 +44,7 @@ void update(int value) {
     }
 
     glutPostRedisplay();
-    glutTimerFunc(16, update, 0);  // 60 FPS
+    glutTimerFunc(10, update, 0);  // 60 FPS
 }
 
 void display() {
@@ -65,10 +62,15 @@ void display() {
 void handleKeypress(unsigned char key, int x, int y) {
     switch (key) {
         case 'a':
-            playerX -= 0.8f;  // Move left
+            if (playerX < -18){
+                isPlayerMoving = false;
+            }
+            else{
+                playerX -= 0.5f;  // Move left
+            }
             break;
         case 'd':
-            playerX += 0.8f;  // Move right
+            playerX += 0.5f;  // Move right
             break;
         case 's':
             if (!isBulletActive) {
@@ -83,8 +85,8 @@ void handleKeypress(unsigned char key, int x, int y) {
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-     glutInitWindowSize(1000, 700);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(1000, 700);
+    glutInitWindowPosition(50, 50);
     glutCreateWindow("Course Shooting Game");
     glutDisplayFunc(display);
     glutKeyboardFunc(handleKeypress);
