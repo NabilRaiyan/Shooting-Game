5f0cf2d976d622fb504317cf5bb808d7d3ca2dda
Raiyan Al Sultan <rraiyan77@gmail.com>
2023-12-20 18:50:12 +0600

    Fixing bugs

--- main.cpp
+++ main.cpp
@@ -307,7 +307,7 @@ void checkPlayerCollision() {
                 playerHealth--;  // Reduce player health
                 std::cout << "Player Health: " << playerHealth << std::endl;
 
-                // Reset player position
+                // Reset player position after collision
                 playerX = -18.0f;
                 playerY = -11.0f;
 
